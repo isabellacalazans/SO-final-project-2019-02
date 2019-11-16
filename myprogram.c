7feb46e51a7544e2f908983f3a1a6fe3c4aaf2a1
assiscalazans <assis.calazans@gmail.com>
2019-11-15 21:16:08 -0300

    Ajuste para threads assincronas (melhor percebido com 100 transferencias simultaneas)

--- myprogram.c
+++ myprogram.c
@@ -1,4 +1,4 @@
-// #define _GNU_SOURCE
+#define _GNU_SOURCE
 #define _OPEN_THREADS
 #include <pthread.h>
 #include <stdlib.h>
@@ -28,9 +28,7 @@ void *transferencia(void *arg)
     from.saldo -= valor;
     to.saldo += valor;
   }
-   printf("Transferência concluída com sucesso!\n");
-   printf("Saldo de c1: %d\n", from.saldo);
-   printf("Saldo de c2: %d\n", to.saldo);
+   printf("Transferência concluída! Saldo c1: %d - Saldo  c2: %d\n" , from.saldo , to.saldo);
    //printf("argumento recebido: %s",arg);
    char *ret;
    ret = (char*) malloc(20);
@@ -39,59 +37,61 @@ void *transferencia(void *arg)
    return 0;
 }
 
-int main() 
+int main()
 {
-	void* stack;
-	void* param;
-	int i;
+ void* stack;
+ void* param;
+ int i;
 
-	// Allocate the stack
-	stack = malloc( FIBER_STACK );
-	if ( stack == 0 )
-	{
-		perror("malloc: could not allocate stack");
-		exit(1);
-	}
+ // Allocate the stack
+ stack = malloc( FIBER_STACK );
+ if ( stack == 0 )
+ {
+ perror("malloc: could not allocate stack");
+ exit(1);
+ }
+ // Todas as contas começam com saldo 100
+ from.saldo = 10000;
+ to.saldo = 10000;
+ printf( "Transferindo 10 para a conta c2\n" );
+ valor = 10;
+ const int limite = 100;
+ pthread_t thid[limite];
+ void * thread_res;
+ int rstatus;
 
-	// Todas as contas começam com saldo 100
-	from.saldo = 100;
-	to.saldo = 100;
-	printf( "Transferindo 10 para a conta c2\n" );
-	valor = 10;
-	pthread_t thid;
-	void * thread_res;
-	int rstatus;
+ for (i = 0; i < limite; i++) {
+   // Call the clone system call to create the child thread
+   //pid = clone( &transferencia, (char*) stack + FIBER_STACK,
+   //SIGCHLD | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_VM, 0 );
 
-	for (i = 0; i < 10; i++) {
-		// Call the clone system call to create the child thread
-		//pid = clone( &transferencia, (char*) stack + FIBER_STACK,
-		//SIGCHLD | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_VM, 0 );
+     pid = pthread_create (&thid[i], NULL, transferencia,"tread 1" );
+     if ( pid != 0 ) {
+       perror( "thread create error" );
+       exit(2);
+     }
 
-		 pid = pthread_create (&thid, NULL, transferencia,"tread 1" );
-		 if ( pid != 0 ) {
-		   perror( "thread create error" );
-		   exit(2);
-		 }
+    printf ("valor de pid %lu \n", thid);;
+ }
+ 
+ for (i = 0; i < limite; i++) {
+	rstatus = pthread_join(thid[i], &thread_res);
 
-		printf ("valor de pid %lu \n", thid);;
+    /*verificar se ocorreu algum erro na chamada de pthread_join*/
+  	if(rstatus != 0)
+  	{
+  		printf ("Erro ao aguardar finalização do thread.\n");
+  		exit(EXIT_FAILURE);
+  	}
 
-		rstatus = pthread_join(thid, &thread_res);
+  	/*exibe o valor de retorno da função 'routine'*/
+  	printf ("Thread finalizado! Retorno = %s\n", (char *)thread_res);
 
-		/*verificar se ocorreu algum erro na chamada de pthread_join*/
-		if(rstatus != 0)
-		{
-			printf ("Erro ao aguardar finalização do thread.\n");
-			exit(EXIT_FAILURE);
-		}
+ }
 
-		/*exibe o valor de retorno da função 'routine'*/
-		printf ("Thread finalizado! Retorno = %s\n", (char *)thread_res);
-
-	}
-
-	//Free the stack
-	free( stack );
-	printf("Transferências concluídas e memória liberada.\n");
-	return 0;
+ // Free the stack
+ free( stack );
+ printf("Transferências concluídas e memória liberada.\n");
+ return 0;
 
 }
